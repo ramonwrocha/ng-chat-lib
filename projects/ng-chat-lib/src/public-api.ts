818963527003e08dfe3e7e9a0d2469b611b53f40
Ramon Souza <ramon.desouza-darocha@viseo.com>
2023-12-06 12:43:38 +0100

    update

--- projects/ng-chat-lib/src/public-api.ts
+++ projects/ng-chat-lib/src/public-api.ts
@@ -5,3 +5,24 @@
 export * from './lib/ng-chat-lib.service';
 export * from './lib/ng-chat-lib.component';
 export * from './lib/ng-chat-lib.module';
+
+export * from './lib/core/chat-adapter';
+export * from './lib/core/message';
+export * from './lib/core/chat-participant-status.enum';
+export * from './lib/core/user';
+export * from './lib/core/participant-response';
+export * from './lib/core/participant-metadata';
+export * from './lib/core/window';
+export * from './lib/core/localization';
+export * from './lib/core/chat-controller';
+export * from './lib/core/paged-history-chat-adapter';
+export * from './lib/core/theme.enum';
+export * from './lib/core/chat-option';
+export * from './lib/core/group';
+export * from './lib/core/chat-participant';
+export * from './lib/core/chat-group-adapter';
+export * from './lib/core/chat-participant-type.enum';
+export * from './lib/core/message-type.enum';
+export * from './lib/core/file-upload-adapter';
+
+export { NgChatLibModule } from './lib/ng-chat-lib.module';
